70a5b8878a7a5cb29aea8b7e807872e4bcad7d7f
xin tang <xtang100@syr.edu>
2020-06-08 22:53:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-display_board = ["", "", "","", "", "", "", "", ""];
+display_board = [];
 
 def display_board
    puts row = ["   " "|" "   " "|" "   "]
